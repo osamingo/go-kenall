206f0bb9f8f95933bfe670a8066c87686cdd1abe
Osamu TONOMORI <osamingo@gmail.com>
2021-02-18 14:59:57 +0900

    Update go version and golangci-lint version

--- kenall_test.go
+++ kenall_test.go
@@ -33,6 +33,7 @@ func TestNewClient(t *testing.T) {
 
 	for name, c := range cases {
 		c := c
+
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
@@ -90,6 +91,7 @@ func TestClient_GetAddress(t *testing.T) {
 
 	for name, c := range cases {
 		c := c
+
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
@@ -126,6 +128,7 @@ func TestVersion_UnmarshalJSON(t *testing.T) {
 
 	for name, c := range cases {
 		c := c
+
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
@@ -163,39 +166,7 @@ func ExampleClient_GetAddress() {
 func runTestingServer(t *testing.T) *httptest.Server {
 	t.Helper()
 
-	const data = `{
-  "version": "2020-11-30",
-  "data": [
-    {
-      "jisx0402": "13101",
-      "old_code": "100",
-      "postal_code": "1008105",
-      "prefecture_kana": "",
-      "city_kana": "",
-      "town_kana": "",
-      "town_kana_raw": "",
-      "prefecture": "東京都",
-      "city": "千代田区",
-      "town": "大手町",
-      "koaza": "",
-      "kyoto_street": "",
-      "building": "",
-      "floor": "",
-      "town_partial": false,
-      "town_addressed_koaza": false,
-      "town_chome": false,
-      "town_multi": false,
-      "town_raw": "大手町",
-      "corporation": {
-        "name": "チッソ　株式会社",
-        "name_kana": "ﾁﾂｿ ｶﾌﾞｼｷｶﾞｲｼﾔ",
-        "block_lot": "２丁目２－１（新大手町ビル）",
-        "post_office": "銀座",
-        "code_type": 0
-      }
-    }
-  ]
-}`
+	const data = `{"version":"2020-11-30","data":[{"jisx0402":"13101","old_code":"100","postal_code":"1008105","prefecture_kana":"","city_kana":"","town_kana":"","town_kana_raw":"","prefecture":"東京都","city":"千代田区","town":"大手町","koaza":"","kyoto_street":"","building":"","floor":"","town_partial":false,"town_addressed_koaza":false,"town_chome":false,"town_multi":false,"town_raw":"大手町","corporation":{"name":"チッソ　株式会社","name_kana":"ﾁﾂｿ ｶﾌﾞｼｷｶﾞｲｼﾔ","block_lot":"２丁目２－１（新大手町ビル）","post_office":"銀座","code_type":0}}]}`
 
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := strings.Fields(r.Header.Get("Authorization"))
